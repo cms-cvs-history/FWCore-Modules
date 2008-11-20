b43be103ad7ad2b94ddcc8be8613484b69a4bdfc
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2008-11-20 05:44:54 +0000

    Synchronize with HEAD

--- src/EventContentAnalyzer.h
+++ src/EventContentAnalyzer.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  Chris Jones
 //         Created:  Mon Sep 19 11:49:35 CEST 2005
-// $Id: EventContentAnalyzer.h,v 1.3 2006/01/10 22:11:16 chrjones Exp $
+// $Id: EventContentAnalyzer.h,v 1.5 2008/11/18 18:51:32 wdd Exp $
 //
 
 // system include files
@@ -28,6 +28,9 @@
 #include "FWCore/Framework/interface/EDAnalyzer.h"
 
 // forward declarations
+namespace edm {
+   class ParameterSetDescription;
+}
 
 class EventContentAnalyzer : public edm::EDAnalyzer {
 public:
@@ -36,6 +39,9 @@ public:
    
    virtual void analyze(const edm::Event&, const edm::EventSetup&);
    virtual void endJob();
+
+   static void fillDescription(edm::ParameterSetDescription& iDesc,
+                               std::string const& moduleLabel);
 
 private:
       
